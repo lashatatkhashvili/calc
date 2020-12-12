c214dc725afbaa2c6547fd050e4b068a13586d5b
unknown <ltatkhashvili@gmail.com>
2020-12-12 14:53:48 +0400

    Updates

---
+++ precache-manifest.b6e6d31d6a2b03a407239cdcc155afe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d680bda3c32253d28c702f3cbb9330e",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "072abde8745f47c162e8",
+    "url": "/calc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a4e747bfbb4c6b1d803c",
+    "url": "/calc/static/js/2.34087315.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc/static/js/2.34087315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072abde8745f47c162e8",
+    "url": "/calc/static/js/main.f65282c5.chunk.js"
+  },
+  {
+    "revision": "3eefc945a4aed9ab2cd7",
+    "url": "/calc/static/js/runtime-main.0a4af9a5.js"
+  }
+]);
